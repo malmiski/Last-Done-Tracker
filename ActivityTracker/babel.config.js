7ae2ecfa642d93d78c3ed09c6f1f38201d5d88e1
T <t@MacBook-Pro.local>
2025-09-18 16:00:59 -0400

    Working prototype

--- ActivityTracker/babel.config.js
+++ ActivityTracker/babel.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-};
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    dependencies: {
+      'react-native-vector-icons': {
+        platforms: {
+          ios: null,
+        },
+      },
+    },
+    presets: ['babel-preset-expo'],
+  };
+};  
